fb5435b8e5ba20408a81c46ed478387692a52c81
kamilahsantos <kamila.fatima@amedigital.com>
2021-08-08 21:04:58 -0300

    extração de metodos repetidos para classes KafkaService,KafkaDispatcher e ConsumerFunction

--- src/main/java/br/com/alura/ecommerce/KafkaService.java
+++ src/main/java/br/com/alura/ecommerce/KafkaService.java
@@ -4,38 +4,47 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
-import java.util.regex.Pattern;
+import java.util.UUID;
 
-public class LogService {
+class KafkaService implements Closeable {
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
 
-    public static void main(String[] args) {
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
-        while(true) {
+    KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    void run() {
+        while (true) {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
                 System.out.println("Encontrei " + records.count() + " registros");
                 for (var record : records) {
-                    System.out.println("------------------------------------------");
-                    System.out.println("LOG:"+ record.topic());
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-
+                    parse.consume(record);
                 }
             }
         }
     }
 
-    private static Properties properties() {
+    private static Properties properties(String groupId) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         return properties;
+    }
+
+    @Override
+    public void close() {
+        consumer.close();
     }
 }

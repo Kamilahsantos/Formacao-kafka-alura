c46f8030e01d5667571f7f7e138f305cb5cf766a
kamilahsantos <kamila.fatima@amedigital.com>
2021-08-15 14:13:29 -0300

    divisão do projeto em microsserviços

--- service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -8,21 +8,17 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try (var orderDispatcher = new KafkaDispatcher<Order>()) {
-            try (var emailDispatcher = new KafkaDispatcher<Email>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
                 for (var i = 0; i < 10; i++) {
 
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
-                    var subject = "compra";
-                    var body = "compra em processamento";
-
 
                     var order = new Order(userId, orderId, amount);
-                    var email = new Email(subject, body);
-
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
+                    var email = "Thank you for your order! We are processing your order!";
                     emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
                 }
             }
